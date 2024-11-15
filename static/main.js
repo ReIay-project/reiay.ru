32b2ec40b4d9ace29cd110b9c6781aac016678de
Friezor <aimeithi@yandex.ru>
2024-11-15 13:48:38 +0700

    Update main.js

--- static/main.js
+++ static/main.js
@@ -17,6 +17,7 @@
     var doc = document.documentElement;
     doc.setAttribute('data-useragent', navigator.userAgent);
 
+    // Preloader
     var ssPreloader = function() {
         $("html").addClass('ss-preload');
 
@@ -29,6 +30,7 @@
         });
     };
 
+    // Info Toggle
     var ssInfoToggle = function() {
         $('.info-toggle').on('click', function(event) {
             event.preventDefault();
@@ -36,6 +38,7 @@
         });
     };
 
+    // Slick Slider
     var ssSlickSlider = function() {
         $('.home-slider').slick({
             arrows: false,
@@ -47,10 +50,12 @@
         });
     };
 
+    // Placeholder Plugin Settings
     var ssPlaceholder = function() {
         $('input, textarea, select').placeholder();
     };
 
+    // Final Countdown
     var ssFinalCountdown = function() {
         var finalDate =  new Date("March 25, 2021 15:37:25").getTime();
         $('.home-content__clock').countdown(finalDate)
@@ -69,6 +74,7 @@
         });
     };
 
+    // AjaxChimp
     var ssAjaxChimp = function() {
         $('#mc-form').ajaxChimp({
             language: 'es',
@@ -76,16 +82,17 @@
         });
 
         $.ajaxChimp.translations.es = {
-            'submit': 'Submitting...',
-            0: '<i class="fas fa-check"></i> We have sent you a confirmation email',
-            1: '<i class="fas fa-exclamation-triangle"></i> You must enter a valid e-mail address.',
-            2: '<i class="fas fa-exclamation-triangle"></i> E-mail address is not valid.',
-            3: '<i class="fas fa-exclamation-triangle"></i> E-mail address is not valid.',
-            4: '<i class="fas fa-exclamation-triangle"></i> E-mail address is not valid.',
-            5: '<i class="fas fa-exclamation-triangle"></i> E-mail address is not valid.'
+            'submit': 'Отправка...',
+            0: '<i class="fas fa-check"></i> Мы отправили вам письмо для подтверждения.',
+            1: '<i class="fas fa-exclamation-triangle"></i> Введите действительный email.',
+            2: '<i class="fas fa-exclamation-triangle"></i> Введите действительный email.',
+            3: '<i class="fas fa-exclamation-triangle"></i> Введите действительный email.',
+            4: '<i class="fas fa-exclamation-triangle"></i> Введите действительный email.',
+            5: '<i class="fas fa-exclamation-triangle"></i> Введите действительный email.'
         }
     };
 
+    // Initialization
     (function ssInit() {
         ssPreloader();
         ssInfoToggle();
@@ -97,6 +104,7 @@
 
 })(jQuery);
 
+// Async request for access form
 async function requestAccess(event) {
     event.preventDefault(); // Предотвращает перезагрузку страницы при отправке формы
 
